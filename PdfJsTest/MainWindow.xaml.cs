2962aa8c538cd54e5eacbd4d516bf69b12a74c0e
Andrei Chirvasie <andrei.chirvasie@issco.ro>
2020-11-03 11:10:58 +0200

    Add project files.

---
+++ PdfJsTest/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PdfJsTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AssetControl.DOMContentLoaded += AssetControl_DOMContentLoaded;
+            AssetControl.NavigationCompleted += AssetControl_Navigated;
+            AssetControl.NavigationCompleted += AssetControl_NavigationCompleted;
+
+
+            var pdfTemplate = "<script src='https://mozilla.github.io/pdf.js/es5/build/pdf.js'></script><div style='text-align: center;'><button id='prev' style='position: fixed; top: 50%; left:5;background-color: transparent;border: none;height: 72px; width: 72px; display:none; cursor: pointer;'><svg viewBox='0 0 20 20' fit='' height='100%' width='100%' preserveAspectRatio='xMidYMid meet' focusable='false'><path d='M14.1808094,17.9991455 L14.1799354,18 C13.8644451,18.3084794 13.3586178,18.3027959 13.0501385,17.9873057 L13.0493966,17.986546 L5.80573614,10.5585334 C5.50273367,10.2478195 5.50273367,9.75218056 5.80573614,9.44146664 L13.0493966,2.01345408 C13.3574564,1.69755413 13.8632757,1.69119843 14.1791757,1.99925822 L14.1799354,2.00000004 L14.1808094,2.00085462 C14.4963777,2.30941029 14.5024479,2.81523899 14.1943752,3.13127874 L7.49880606,10 L14.1943752,16.8687213 C14.5024479,17.1847611 14.4963777,17.6905898 14.1808094,17.9991455 Z'></path></svg></button><canvas id='the-canvas' width='1123px' height='558px'></canvas><button id='next' style='position: fixed; top: 50%; right:5; background-color: transparent;border: none;height: 72px; width: 72px; display:none; cursor: pointer;'><svg viewBox='0 0 20 20' fit='' height='100%' width='100%' preserveAspectRatio='xMidYMid meet' focusable='false' style='color:transparent;'><path d='M5.81919054,17.9991455 C5.50362224,17.6905898 5.49755199,17.1847611 5.80562475,16.8687213 L12.5011939,10 L5.80562475,3.13127874 C5.49755199,2.81523899 5.50362224,2.30941029 5.81919054,2.00085462 L5.82006454,2.00000004 C5.82031761,1.9997526 5.82057084,1.99950532 5.82082423,1.99925822 C6.13672418,1.69119843 6.64254349,1.69755413 6.95060328,2.01345408 L14.1942638,9.44146664 C14.4972662,9.75218056 14.4972662,10.2478195 14.1942638,10.5585334 L6.95060328,17.986546 C6.95035617,17.9867994 6.9501089,17.9870526 6.94986145,17.9873057 C6.6413821,18.3027959 6.13555479,18.3084794 5.82006454,18 L5.81919054,17.9991455 Z'></path></svg></button></div>";
+            AssetControl.NavigateToString(pdfTemplate);
+        }
+
+        private async void AssetControl_NavigationCompleted(object sender, WebViewControlNavigationCompletedEventArgs e)
+        {
+            await InjectScript("pdfHelper.js");
+            var pdfContent = Convert.ToBase64String(File.ReadAllBytes("test.pdf"));
+            AssetControl.InvokeScript("openPdf", pdfContent, "", "");
+        }
+
+        private void AssetControl_Navigated(object sender, WebViewControlNavigationCompletedEventArgs e)
+        {
+           // throw new NotImplementedException();
+        }
+
+        private void AssetControl_DOMContentLoaded(object sender, WebViewControlDOMContentLoadedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private async Task InjectScript(string path)
+        {
+            var scriptContent = File.ReadAllText(path);
+            await AssetControl.InvokeScriptAsync("eval", new string[] { scriptContent });
+        }
+    }
+}
---
+++ PdfJsTest/pdfHelper.js
@@ -0,0 +1,276 @@
+﻿function openPdf(contents, pages, annotations) {
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    pages = pages.split(',').map(Number);
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/es5/build/pdf.worker.js';
+    var pdfDoc = null,
+        pageNum = 1,
+        pageRendering = false,
+        pageNumPending = null,
+        scale = 1,
+        canvas = document.getElementById('the-canvas'),
+        ctx = canvas.getContext('2d'),
+        canvasWidth = canvas.width,
+        TEXT_LINE_HEIGHT = 1.2,
+        TEXT_FONT_FAMILY = '"Noto Sans", sans-serif',
+        TYPE_COLOR_ALPHA = {};
+
+    TYPE_COLOR_ALPHA['PEN'] = 1;
+    TYPE_COLOR_ALPHA['HIGHLIGHTER'] = 0.5;
+    TYPE_COLOR_ALPHA['TEXT'] = 1;
+
+    if (pages[0] != 0) {
+        pageNum = pages[0];
+    }
+
+    var pdfData = atob(contents);
+    var annotationsData = annotations;
+    var loadingTask = pdfjsLib.getDocument({ data: pdfData });
+    loadingTask.promise.then(function (pdf) {
+        pdfDoc = pdf;
+        renderPage(pageNum);
+    });
+
+    function renderPage(num) {
+        pageRendering = true;
+        pdfDoc.getPage(num).then(function (page) {
+
+            var viewport = page.getViewport({ scale: canvasWidth / page.getViewport({ scale: scale }).width });
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            var renderContext = {
+                canvasContext: ctx,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+
+            renderTask.promise.then(function () {
+                pageRendering = false;
+
+                if (pages[0] != 0) {
+                    lastPage = pages[pages.length - 1];
+                } else {
+                    pages = numberRange(1, pdfDoc.numPages);
+                    lastPage = pdfDoc.numPages;
+                }
+
+                var prevPage = num - 1;
+                if (prevPage < pages[0]) {
+                    document.getElementById('prev').style.display = 'none';
+                } else {
+                    document.getElementById('prev').style.display = 'block';
+                }
+                var nextPage = num + 1;
+                if (nextPage > lastPage) {
+                    document.getElementById('next').style.display = 'none';
+                } else {
+                    document.getElementById('next').style.display = 'block';
+                }
+
+                ctx.lineCap = 'round';
+                ctx.lineJoin = 'round';
+                ctx.miterLimit = 10;
+
+                renderAnnotationLines(annotationsData);
+                renderAnnotationText(annotationsData);
+
+                if (pageNumPending !== null) {
+                    renderPage(pageNumPending);
+                    pageNumPending = null;
+                }
+
+            });
+        });
+    }
+
+    function renderAnnotationLines(annotationsData) {
+        var annotationsJson = JSON.parse(annotationsData);
+        var annotationItemModels = annotationsJson.find(x => x.Page === pageNum - 1);
+
+        if (annotationItemModels) {
+            var annotationsLine = annotationItemModels.Data.filter(isLine);
+            if (annotationsLine) {
+                ctx.lineCap = 'round';
+                ctx.lineJoin = 'round';
+                ctx.miterLimit = 10;
+
+                for (var i = 0; i < annotationsLine.length; i++) {
+                    annotationsLine[i].Points = annotationsLine[i].points;
+                    var points = annotationsLine[i].Points;
+                    if (points.length > 0) {
+                        var point = points[0];
+                        var x = point[0] * canvas.width;
+                        var y = point[1] * canvas.height;
+
+                        ctx.beginPath();
+                        ctx.moveTo(x, y);
+
+                        var color = annotationsLine[i].color;
+                        var size = annotationsLine[i].size;
+                        // Make sure we can draw dots from single lines
+                        ctx.lineTo(x + 0.01, y + 0.01);
+
+                        for (var index = 1; index < annotationsLine[i].points.length; index++) {
+                            var point = annotationsLine[i].points[index];
+                            var x = point[0] * canvas.width;
+                            var y = point[1] * canvas.height;
+
+                            ctx.lineTo(x, y);
+                        }
+                        ctx.strokeStyle = "#" + color;
+                        ctx.lineWidth = parseFloat(size) * canvas.width;
+                        ctx.globalAlpha = TYPE_COLOR_ALPHA[annotationsLine[i].type];
+
+                        ctx.stroke();
+                    }
+                }
+            }
+        }
+    }
+
+    function renderAnnotationText(annotations) {
+        var annotationsJson = JSON.parse(annotations);
+        var annotationItemModels = annotationsJson.find(x => x.Page === pageNum - 1);
+        if (annotationItemModels) {
+            var annotationsText = annotationItemModels.Data.filter(isText);
+            for (var i = 0; i < annotationsText.length; i++) {
+
+                var points = annotationsText[i].points;
+
+                if (points.length !== 2) {
+                    throw new Error('Text annotation expects 2 points');
+                }
+
+                var x1 = points[0][0] * canvas.width;
+                var y1 = points[0][1] * canvas.height;
+                var x2 = points[1][0] * canvas.width;
+                var maxWidth = x2 - x1;
+
+                var size = annotationsText[i].size * canvas.width;
+                var offsetTop = y1;
+                var text = void 0;
+                var textHeight = 0;
+                var line = void 0;
+
+                function getNextLine() {
+
+                    text = text.trim();
+
+                    var lines = text.split('\n');
+                    var line = lines[0];
+                    var lineWidth = ctx.measureText(line).width;
+
+                    if (lineWidth > maxWidth) {
+                        while (line.indexOf(' ') > -1 && lineWidth > maxWidth) {
+                            line = line.substr(0, line.lastIndexOf(' '));
+                            lineWidth = ctx.measureText(line).width;
+                        }
+
+                        while (lineWidth > maxWidth && line.length > 1) {
+                            line = line.substr(0, line.length - 1);
+                            lineWidth = ctx.measureText(line).width;
+                        }
+
+                        // update remaining text
+                        lines[0] = lines[0].substr(line.length);
+                        text = lines.join('\n');
+                    } else {
+                        // update remaining text
+                        lines.shift();
+                        text = lines.join('\n');
+                    }
+
+                    return line;
+                }
+
+                text = annotationsText[i].text;
+                line = getNextLine();
+
+                while (typeof line === 'string' && line.length > 0) {
+                    textHeight += TEXT_LINE_HEIGHT * size;
+
+                    line = getNextLine();
+                }
+
+                ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
+                ctx.fillRect(x1, y1, x2 - x1, textHeight);
+
+                ctx.globalAlpha = 1;
+                ctx.font = size + 'px ' + TEXT_FONT_FAMILY.split(',')[0].trim();
+                ctx.fillStyle = '#' + annotationsText[i].color;
+                ctx.fontFamily = TEXT_FONT_FAMILY;
+
+                text = annotationsText[i].text;
+                line = getNextLine();
+                while (typeof line === 'string' && line.length > 0) {
+                    ctx.fillText(line, x1, offsetTop + size);
+                    offsetTop += TEXT_LINE_HEIGHT * size;
+                    line = getNextLine();
+                }
+            }
+        }
+    }
+
+    function numberRange(start, end) {
+        return new Array(end - start + 1).fill().map((d, i) => i + start);
+    }
+
+    function isLine(x) {
+        return x.type !== "TEXT";
+    }
+
+    function isText(x) {
+        return x.type === "TEXT";
+    }
+
+    function getNextPage(num) {
+        index = pages.indexOf(num);
+        if (index >= 0 && index < pages.length - 1) {
+            num = pages[index + 1];
+        }
+        return num;
+    }
+
+    function getPrevPage(num) {
+        index = pages.indexOf(num);
+        if (index > 0 && index < pages.length) {
+            num = pages[index - 1];
+        }
+        return num;
+    }
+
+    function queueRenderPage(num) {
+        if (pageRendering) {
+            pageNumPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+
+    function onPrevPage() {
+        if (pages[0] != 0) {
+            pageNum = getPrevPage(pageNum)
+        } else {
+            if (pageNum <= 1) {
+                return;
+            }
+            pageNum--;
+        }
+        queueRenderPage(pageNum);
+    }
+    document.getElementById('prev').addEventListener('click', onPrevPage);
+
+    function onNextPage() {
+        if (pages[0] != 0) {
+            pageNum = getNextPage(pageNum)
+        } else {
+            if (pageNum >= pdfDoc.numPages) {
+                return;
+            }
+            pageNum++;
+        }
+        queueRenderPage(pageNum);
+    }
+    document.getElementById('next').addEventListener('click', onNextPage);
+}
